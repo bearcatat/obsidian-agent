985ee99ae24bfedc0f0599c1c3389ece107cedcb
Gan Ziyu <ziyu.gan@shopee.com>
2026-02-17 20:38:18 +0800

    chore: Remove unused ContainerContext
    - Delete src/hooks/container-context.tsx which had no consumers
    - Update settings-tab.tsx to remove ContainerContext.Provider usage
    - Settings component still works without this unused context

--- src/ui/components/settings/setttings-tab.tsx
+++ src/ui/components/settings/setttings-tab.tsx
@@ -2,7 +2,6 @@ import { App, PluginSettingTab } from "obsidian";
 import { IObsidianAgentPlugin } from "../../../types";
 import { createRoot } from "react-dom/client";
 import { Settings } from "./settings";
-import { ContainerContext } from "../../../hooks/container-context";
 import { TabProvider } from "../../../hooks/TabContext";
 
 export class ObsidianAgentSettingTab extends PluginSettingTab {
@@ -21,11 +20,9 @@ export class ObsidianAgentSettingTab extends PluginSettingTab {
         const sections = createRoot(div);
 
         sections.render(
-            <ContainerContext.Provider value={containerEl}>
-                <TabProvider>
-                    <Settings />
-                </TabProvider>
-            </ContainerContext.Provider>
+            <TabProvider>
+                <Settings />
+            </TabProvider>
         );
     }
 }
